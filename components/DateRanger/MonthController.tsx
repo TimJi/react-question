4cb9646a672aa0aae63ae6551e6d6a7b00d476f6
Tim Ji <timji@kktv.me>
2024-06-05 00:36:42 +0800

    implement MonthController

--- components/DateRanger/MonthController.tsx
+++ components/DateRanger/MonthController.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 interface MonthControllerProps {
   children: React.ReactNode;
+  onClick?: () => void;
 }
 
-export const MonthController = ({ children }: MonthControllerProps) => {
+export const MonthController = ({ children, onClick }: MonthControllerProps) => {
   return (
-    <div className="w-[50px] hover:bg-[#e6e6e6] text-center">
+    <div
+      className="w-[50px] hover:bg-[#e6e6e6] text-center cursor-pointer select-none"
+      onClick={onClick}
+    >
       {children}
     </div>
   )
--- components/DateRanger/index.tsx
+++ components/DateRanger/index.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import DataCell from '@/components/DateRanger/DataCell'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MonthController } from '@/components/DateRanger/MonthController'
+import dayjs from 'dayjs'
 
 interface DataRangerProps {
   prevMonthDays: string[];
@@ -11,20 +12,33 @@ interface DataRangerProps {
 }
 
 const DateRanger: React.FC<DataRangerProps> = ({ prevMonthDays, nextMonthDays, currentMonthDays }) => {
-  const [year, setYear] = useState('2022')
-  const [month, setMonth] = useState('7')
+  const [year, setYear] = useState(dayjs().format('YYYY'))
+  const [month, setMonth] = useState(dayjs().format('M'))
+  const [yearMonth, setYearMonth] = useState(dayjs().format('YYYY-MM'))
+
+  let onMonthIncrement = () => {
+    setYearMonth(dayjs(yearMonth).add(1, 'month').format('YYYY-MM'))
+  }
+  let onMonthDecrement = () => {
+    setYearMonth(dayjs(yearMonth).subtract(1, 'month').format('YYYY-MM'))
+  }
+
+  useEffect(() => {
+    setYear(dayjs(yearMonth).format('YYYY'))
+    setMonth(dayjs(yearMonth).format('M'))
+  }, [yearMonth])
 
   return (
     <div className="flex justify-center text-base">
       <div className="w-[350px] h-[210px]">
         <div className="flex h-11 text-center mb-4 leading-11">
-          <MonthController>{'<'}</MonthController>
+          <MonthController onClick={onMonthDecrement}>{'<'}</MonthController>
           <div className="flex-grow text-center">{`${year}年${month}月`}</div>
-          <MonthController>{'>'}</MonthController>
+          <MonthController onClick={onMonthIncrement}>{'>'}</MonthController>
         </div>
         <div className="flex w-[350px] flex-wrap">
           {prevMonthDays.map((date, index) => (
-            <DataCell date={date} key={index} isNonCurrentMonth/>
+            <DataCell date={date} key={index} isNonCurrentMonth />
           ))}
           {currentMonthDays.map((date, index) => (
             <DataCell date={date} key={index} />
